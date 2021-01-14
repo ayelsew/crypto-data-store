36354cf83a55abe01ed88bda88cac1c9533a1064
wesley <wesley.waaraujo@gmail.com>
2021-01-13 23:04:02 -0300

    update eslit airbnb-typescript

--- src/interfaces/Cryptography.ts
+++ src/interfaces/Cryptography.ts
@@ -1,10 +1,10 @@
 export type DataRaw = string |
-  Uint8Array |
-  Uint8ClampedArray |
-  Uint16Array |
-  Uint32Array |
-  Int8Array |
-  Int16Array;
+Uint8Array |
+Uint8ClampedArray |
+Uint16Array |
+Uint32Array |
+Int8Array |
+Int16Array;
 
 export interface ICryptography {
   encrypt(data: DataRaw): Buffer;
